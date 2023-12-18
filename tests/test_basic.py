0491b8712702af44d8eb372cf40576013e7212fb
Christopher MacLellan <cmaclell@gatech.edu>
2023-12-18 17:30:32 -0500

    working nox run

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-import scikit_build_example as m
+import fast_cobweb as m
 
 
 def test_version():
-    assert m.__version__ == "0.0.1"
+    assert m.__version__ == "0.1.0"
 
 
 def test_add():
